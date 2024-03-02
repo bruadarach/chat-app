6b2aed3116d19247ee5266fb1848d66160bcbe32
Sujeong Ji <suji.sujeongji@gmail.com>
2024-03-02 18:28:49 +0900

    feat: add cookie-parser middleware and message route

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 export default app;
